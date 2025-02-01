0f4e036a200c5c993a8f00ec0cb1a1fc161f83d9
Vobh <vobh_malino@hotmail.com>
2025-02-01 10:59:32 -0300

    Commit 0.1.init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+      <main className="">
+        Natureggae
+
+        <h2 className="text-natureggae">Code my pick up</h2>
+      </main>
+  );
+}
