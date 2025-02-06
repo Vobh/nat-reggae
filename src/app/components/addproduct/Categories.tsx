e3389e1acf006750fb2706aa7b8fa77c4b99932d
Vobh <vobh_malino@hotmail.com>
2025-02-06 12:56:58 -0300

    Continue building add product client

---
+++ src/app/components/addproduct/Categories.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+interface CategoriesProps {
+    dataCategory: string;
+    setCategory: (category: string) => void;
+}
+
+const Categories: React.FC<CategoriesProps> = ({
+    dataCategory,
+    setCategory      
+}) => {
+    return (
+        <>
+            <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+                <div 
+                    onClick={() => setCategory('beach')}
+                    className={`pb-4 flex flex-col font-bold items-center space-y-2 border-b-2 ${dataCategory == 'beach' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                >
+                    <Image 
+                        src="/icn_category_beach.jpeg"
+                        alt="Category - Beach"
+                        width={30}
+                        height={30}
+                    />
+                    <span className="text-sm">Beach</span>
+                </div>
+    
+                <div 
+                    onClick={() => setCategory('villas')}
+                    className={`pb-4 flex flex-col font-bold items-center space-y-2 border-b-2 ${dataCategory == 'villas' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                >
+                    <Image 
+                        src="/icn_category_beach.jpeg"
+                        alt="Category - Beach"
+                        width={30}
+                        height={30}
+                    />
+                    <span className="text-sm">Villas</span>
+                </div>
+    
+                <div 
+                    onClick={() => setCategory('cabin')}
+                    className={`pb-4 flex flex-col font-bold items-center space-y-2 border-b-2 ${dataCategory == 'cabin' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                >
+                    <Image 
+                        src="/icn_category_beach.jpeg"
+                        alt="Category - Beach"
+                        width={30}
+                        height={30}
+                    />
+                    <span className="text-sm">Cabin</span>
+                </div>
+    
+                <div 
+                    onClick={() => setCategory('tiny homes')}
+                    className={`pb-4 flex flex-col font-bold items-center space-y-2 border-b-2 ${dataCategory == 'tiny homes' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                >
+                    <Image 
+                        src="/icn_category_beach.jpeg"
+                        alt="Category - Beach"
+                        width={30}
+                        height={30}
+                    />
+                    <span className="text-sm">Tiny homes</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Categories;
--- src/app/components/modals/AddProduct.tsx
+++ src/app/components/modals/AddProduct.tsx
@@ -6,24 +6,166 @@ import Modal from "./modal";
 
 import useAddProduct from "@/app/hooks/useAddProduct";
 import CustomButton from "../forms/CustomButton";
+import Categories from "../addproduct/Categories";
+import SelectCountry, { SelectCountryValue } from "../forms/SelectCountry";
 
-const AddProduct = () => { // 6:08:41
+const AddProduct = () => {
+    //
+    // States
     const [currentStep, setCurrentStep] = useState(1);
+    const [dataCategory, setDataCategory] = useState('');
+    const [dataTitle, setDataTitle] = useState('');
+    const [dataDescription, setDataDescription] = useState('');
+    const [dataPrice, setDataPrice] = useState('');
+    const [dataDays, setDataDays] = useState('');
+    const [dataCountry, setDataCountry] = useState<SelectCountryValue>();
+
+    //
+    //
 
     const addProduct = useAddProduct();
+
+    //
+    // Set datas
+
+    const setCategory = (category: string) => {
+        setDataCategory(category)
+    }
+
+    //
+    //
 
     const content = (
         <>
             {currentStep == 1 ? (
                 <>
                     <h2 className="mb-6 text-2xl">Choose category</h2>
+                    <div className="w-full p-4 mb-4 bg-purple-100 hover:bg-white border border-gray-600 rounded-xl">
+                        <Categories 
+                            dataCategory={dataCategory}
+                            setCategory={(category) => setCategory(category)}
+                        />
+                    </div>
+
                     <CustomButton 
                         label="Next"
                         onClick={() => setCurrentStep(2)}
                     />
                 </>
+            ) : currentStep == 2 ? (
+                <>
+                    <h2 className="mb-6 text-2xl">Describe your show</h2>
+
+                    <div className="pt-3 pb-6 space-y-4">
+                        <div className="flex flex-col space-y-2">
+                            <label>Title</label>
+                            <input
+                                type="text"
+                                value={dataTitle}
+                                onChange={(e) => setDataTitle(e.target.value)}
+                                className="w-full p-4 border border-gray-600 rounded-xl"
+                            />
+                        </div>
+
+                        <div className="flex flex-col space-y-2">
+                            <label>Description</label>
+                            <textarea
+                                value={dataDescription}
+                                onChange={(e) => setDataDescription(e.target.value)}
+                                className="w-full h-[200px] p-4 border border-gray-600 rounded-xl"
+                            ></textarea>
+                        </div>
+                    </div>
+
+                    <CustomButton 
+                        label="Previous"
+                        className="mb-2 bg-black hover:bg-gray-800"
+                        onClick={() => setCurrentStep(1)}
+                    />
+                    
+                    <CustomButton 
+                        label="Next"
+                        onClick={() => setCurrentStep(3)}
+                    />
+                </>
+            ) : currentStep == 3 ? (
+                <>
+                    <h2 className="mb-6 text-2xl">Details</h2>
+
+                    <div className="pt-3 pb-6 space-y-4">
+                        <div className="flex flex-col space-y-2">
+                            <label>Price per nigth</label>
+                            <input
+                                type="number"
+                                value={dataPrice}
+                                onChange={(e) => setDataPrice(e.target.value)}
+                                className="w-full p-4 border border-gray-600 rounded-xl"
+                            />
+                        </div>
+
+                        <div className="flex flex-col space-y-2">
+                            <label>Days</label>
+                            <input
+                                type="number"
+                                value={dataDays}
+                                onChange={(e) => setDataDays(e.target.value)}
+                                className="w-full p-4 border border-gray-600 rounded-xl"
+                            />
+                        </div>
+                    </div>
+
+                    <CustomButton 
+                        label="Previous"
+                        className="mb-2 bg-black hover:bg-gray-800"
+                        onClick={() => setCurrentStep(2)}
+                    />
+                    
+                    <CustomButton 
+                        label="Next"
+                        onClick={() => setCurrentStep(4)}
+                    />
+                </>
+            ) : currentStep == 4 ? (
+                <>
+                    <h2 className="mb-6 text-2xl">Location</h2>
+
+                    <div className="pt-3 pb-6 space-y-4">
+                        <SelectCountry
+                            value={dataCountry}
+                            onChange={(value) => setDataCountry(value as SelectCountryValue)}
+                        />
+                    </div>
+                    
+                    <CustomButton 
+                        label="Previous"
+                        className="mb-2 bg-black hover:bg-gray-800"
+                        onClick={() => setCurrentStep(3)}
+                    />
+                    
+                    <CustomButton 
+                        label="Next"
+                        onClick={() => setCurrentStep(5)}
+                    />
+                </>
             ) : (
-                <p>step 2</p>
+                <>
+                    <h2 className="mb-6 text-2xl">Image</h2>
+
+                    <div className="pt-3 pb-6 space-y-4">
+                        <>6:46:06</>
+                    </div>
+
+                    <CustomButton 
+                        label="Previous"
+                        className="mb-2 bg-black hover:bg-gray-800"
+                        onClick={() => console.log('Submit')}
+                    />
+                    
+                    <CustomButton 
+                        label="Submit"
+                        onClick={() => setCurrentStep(5)}
+                    />
+                </>
             )}
         </>
     )
